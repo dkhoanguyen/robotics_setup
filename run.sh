dcd15532449020af71aa73bc772ec052d27875f0
dkhoanguyen <khoanguyendacdang2198@gmail.com>
2023-04-21 11:34:29 +1000

    Added calibration script

--- run.sh
+++ run.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 
-docker run -it 
-    --name="ur3e_controller" \
-    --privileged="true" \
-    --restart="always" \
-    --network="host" \
+# TODO: check whether this folder exists
+mkdir calibration_file
+
+# Calibrate the robot
+docker run -d -rm --name "ur3e_calibration" \
+    --privileged \
+    --network "host" \
+    --calibration_file:/calibration_file \
+    robotic_base:latest \
+    "/bin/bash -c source /ur_ws/devel/setup.bash &&\
+    timeout 20 roslaunch ur_calibration calibration_correction.launch robot_ip:=150.22.0.41 target_filename:='/calibration_file/ur3e_calibration.yaml'"
+
+docker run -it --name "ur3e_controller" \
+    --privileged \
+    --restart "always" \
+    --network "host" \
+    --calibration_file:/calibration_file \
     robotic_base:latest bash
--- scripts/ros_entrypoint.sh
+++ scripts/ros_entrypoint.sh
@@ -3,5 +3,5 @@ set -e
 
 # setup ros environment
 source "/opt/ros/$ROS_DISTRO/setup.bash"
-source "/root/ur_ws/devel/setup.bash"
+source "/ur_ws/devel/setup.bash"
 exec "$@"
