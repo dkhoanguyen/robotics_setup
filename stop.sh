97889897fc8f62c3d1357148c76c6e7398406899
dkhoanguyen <khoanguyendacdang2198@gmail.com>
2023-08-01 17:55:50 +1000

    Force remove wifiap first to prevent shutting down

--- stop.sh
+++ stop.sh
@@ -5,6 +5,8 @@ running_containers=$(docker ps --format '{{.ID}}')
 
 if [ -n "$running_containers" ]; then
     echo "There are running containers."
+    # Force remove wifi ap first to prevent shutting dow
+    docker rm -f rpi3-wifiap
     # Stop all running containers
     docker stop $(docker ps -q)
 
