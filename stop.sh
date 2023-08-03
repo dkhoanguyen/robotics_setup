32553938410f199356a13c91b61914b59233915f
dkhoanguyen <khoanguyendacdang2198@gmail.com>
2023-08-03 11:05:45 +1000

    stop script should only stop robot controller

--- stop.sh
+++ stop.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
 # Get the number of running containers
-running_containers=$(docker ps --format '{{.ID}}')
+running_containers=$(docker ps --format '{{.Names}}')
 
 if [ -n "$running_containers" ]; then
-    echo "There are running containers."
-    # Force remove wifi ap first to prevent shutting dow
-    docker rm -f rpi3-wifiap
-    # Stop all running containers
-    docker stop $(docker ps -q)
-
-    # Remove all stopped containers
-    docker rm $(docker ps -a -q)
+    for container in "$running_containers"
+    do
+        if [[ "$container" == "rosbridge" || "$container" == "rpi3-wifiap" ]]; then
+            continue
+        else
+            docker stop "$container"
+            docker rm "$container"
+        fi
+    done
 fi
