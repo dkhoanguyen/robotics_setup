95b59c4fb304de97adb255d7091ce000bd4dee65
dkhoanguyen <khoanguyendacdang2198@gmail.com>
2023-08-03 11:17:37 +1000

    Update stop script

--- stop.sh
+++ stop.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-# Get the number of running containers
-running_containers=$(docker ps --format '{{.Names}}')
+# Add the container names you want to keep to this array
+container_names_to_keep=("rosbridge" "rpi3-wifiap" )
 
-if [ -n "$running_containers" ]; then
-    for container in "$running_containers"
-    do
-        if [[ "$container" == "rosbridge" || "$container" == "rpi3-wifiap" ]]; then
-            continue
-        else
-            docker stop "$container"
-            docker rm "$container"
-        fi
-    done
+# Get the IDs of all running containers (excluding the ones to keep)
+container_ids_to_stop=$(docker ps -q --filter "name!=${container_names_to_keep[*]}")
+
+# Stop the containers (if any)
+if [[ -n "$container_ids_to_stop" ]]; then
+    docker stop $container_ids_to_stop
+    docker rm $container_ids_to_stop
+    echo "Stopped all containers except: ${container_names_to_keep[*]}."
+else
+    echo "No containers to stop except: ${container_names_to_keep[*]}."
 fi
