75071af3a36696e0275e7a7ab8247caae2b3c3dc
dkhoanguyen <khoanguyendacdang2198@gmail.com>
2023-08-03 11:19:41 +1000

    Update stop script again

--- stop.sh
+++ stop.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 
 # Add the container names you want to keep to this array
-container_names_to_keep=("rosbridge" "rpi3-wifiap" )
+container_names_to_keep=("container_name1" "container_name2" "container_name3")
 
-# Get the IDs of all running containers (excluding the ones to keep)
-container_ids_to_stop=$(docker ps -q --filter "name!=${container_names_to_keep[*]}")
+# Get all running container IDs
+all_container_ids=$(docker ps -q)
+
+# Loop through the container IDs and check if their names are in the "to keep" list
+container_ids_to_stop=()
+for container_id in $all_container_ids; do
+    container_name=$(docker inspect -f '{{.Name}}' "$container_id" | sed 's,^/,,')
+    if [[ ! " ${container_names_to_keep[@]} " =~ " ${container_name} " ]]; then
+        container_ids_to_stop+=("$container_id")
+    fi
+done
 
 # Stop the containers (if any)
-if [[ -n "$container_ids_to_stop" ]]; then
-    docker stop $container_ids_to_stop
-    docker rm $container_ids_to_stop
+if [[ ${#container_ids_to_stop[@]} -gt 0 ]]; then
+    docker stop "${container_ids_to_stop[@]}"
     echo "Stopped all containers except: ${container_names_to_keep[*]}."
 else
     echo "No containers to stop except: ${container_names_to_keep[*]}."
