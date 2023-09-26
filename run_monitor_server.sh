9e4a847c115a1047d3d761ec5e5746b15c833b64
dkhoanguyen <khoanguyendacdang2198@gmail.com>
2023-09-26 23:05:55 +1000

    Update run file

--- run_monitor_server.sh
+++ run_monitor_server.sh
@@ -10,5 +10,5 @@ docker run -d --name "robot_monitor_server" \
     --volume /run/systemd:/run/systemd --pid=host \
     --mount type=bind,source="$(pwd)"/calibration_file,target=/calibration_file \
     monitor_server:latest \
-    bash -c "cd /robots_monitor_server/ && \
+    bash -c "cd /robots_monitor_server/src/ && \
              gunicorn --bind 0.0.0.0:8080 -t 60 main:app"
