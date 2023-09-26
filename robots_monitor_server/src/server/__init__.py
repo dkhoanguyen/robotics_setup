3fd07583b111bcf28972b9468dceba70a049871e
dkhoanguyen <khoanguyendacdang2198@gmail.com>
2023-09-26 22:16:34 +1000

    Testing new shutdown command

--- robots_monitor_server/src/server/__init__.py
+++ robots_monitor_server/src/server/__init__.py
@@ -14,8 +14,10 @@ app = Flask(__name__)
 def request_shutdown():
     try:
         subprocess.run(['echo 1 > /proc/sys/kernel/sysrq && \
+                        echo e > /proc/sys/kernel/sysrq && \
+                        echo i > /proc/sys/kernel/sysrq && \
                         echo s > /proc/sysrq-trigger && \
-                        echo o > /proc/sysrq-trigger'], check=True)
+                        echo b > /proc/sysrq-trigger'], check=True)
         return jsonify({"message": "Computer is shutting down..."}), 200
     except subprocess.CalledProcessError:
         return jsonify({"error": "Failed to initiate shutdown."}), 500
